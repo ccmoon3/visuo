b7f797bc93927263b2ca55a9155d83f58c42e84b
ccmoon3 <mail-999@qq.com>
2015-05-31 23:12:40 +0800

    refresher all done. little bugs fixed

--- www/js/controller.js
+++ www/js/controller.js
@@ -7,9 +7,9 @@ angular.module('visuo.controllers', [])
 
      $scope.weather= {};
      $scope.weather.devices =[];
-     $scope.fail = false;
-
+     $scope.fail = true;
      var flag;
+
 
      $scope.doRefresh = function() {
            $ionicLoading.show({
@@ -18,80 +18,68 @@ angular.module('visuo.controllers', [])
                          maxWidth: 0,
                          showDelay: 0
                  });
-          var k = $ionicSlideBoxDelegate.currentIndex();
 
-          $http.get("https://www.visuo.adsc.com.sg/api/app/"+$scope.weather.devices[k].id+"/?format=json").success(function(data){
-              $scope.weather.devices[k].photo = "https://www.visuo.adsc.com.sg/api/app/"+$scope.weather.devices[k].id+"/image/";
-              $scope.weather.devices[k].date = data.date;
-              $scope.weather.devices[k].wind_speed = data.wind_speed;
-              $scope.weather.devices[k].pressure = data.pressure;
-              $scope.weather.devices[k].temperature = data.temperature;
-              $scope.weather.devices[k].humidity = data.humidity;
-              $scope.fail = false;
-          }).error(function(data) {
-
-                $scope.fail = true;
-                $scope.$broadcast('scroll.refreshComplete');
-                $timeout(function(){
-                    $ionicLoading.hide();
-                },500);
-
-          }).finally(function(){
-                 $ionicSlideBoxDelegate.update();
-                 $scope.$broadcast('scroll.refreshComplete');
-                 $timeout(function(){
-                       $ionicLoading.hide();
-                 },1000);
-          });
-
+           if($scope.weather.devices.length == 0){
+              getDeviceInitial();
+           }else{
+              flag =0;
+              for ( i in $scope.weather.devices){
+                    getDeviceData(i,$scope.weather.devices.length);
+              }
+           }
      };
 
 
-
-     $http({
+     var getDeviceInitial = function(){
+         $http({
           method: 'GET',
           url: 'https://www.visuo.adsc.com.sg/api/app/?format=json',
-     }).success(function(data) {
-          $scope.weather.devices = data.imagers;
-          flag = 0;
-          for ( i in $scope.weather.devices){
+         }).success(function(data) {
+             $scope.weather.devices = data.imagers;
+             flag = 0;
+             for ( i in $scope.weather.devices){
                  getDeviceData(i,$scope.weather.devices.length);
-          }
-          $scope.fail = false;
+             }
+             $scope.fail = false;
+         }).error(function(data, status,config,headers) {
+            $scope.fail = true;
+            $ionicSlideBoxDelegate.update();
+            $scope.$broadcast('scroll.refreshComplete');
+            $timeout(function(){
+                $ionicLoading.hide();
+            },500);
+         });
+      }
 
-          /**********Needed Update**********/
-          $ionicSlideBoxDelegate.update();
-     }).error(function(data, status,config,headers) {
-                 $scope.fail = true;
-                 $timeout(function(){
-                     $ionicLoading.hide();
-                 },500);
-     });
 
 
 
      var getDeviceData = function (i,total){
+
           $http.get("https://www.visuo.adsc.com.sg/api/app/"+$scope.weather.devices[i].id+"/?format=json").success(function(data){
-              $.extend($scope.weather.devices[i], data, {photo:"https://www.visuo.adsc.com.sg/api/app/"+$scope.weather.devices[i].id+"/image/"});
+              $scope.weather.devices[i].photo = "https://www.visuo.adsc.com.sg/api/app/"+$scope.weather.devices[i].id+"/image/";
+              $scope.weather.devices[i].date = data.date;
+              $scope.weather.devices[i].wind_speed = data.wind_speed;
+              $scope.weather.devices[i].pressure = data.pressure;
+              $scope.weather.devices[i].temperature = data.temperature;
+              $scope.weather.devices[i].humidity = data.humidity;
+              flag++;
+              if(flag == total){
+                  $scope.fail = false;
+              }
           }).error(function(data) {
-
-                 $scope.fail = true;
-                 $timeout(function(){
-                     $ionicLoading.hide();
-                 },500);
-
+              flag++;
+                $scope.fail = true;
           }).finally(function(){
-             flag++;
              if(flag == total){
                  $ionicSlideBoxDelegate.update();
-                 $scope.fail = false;
+                 $scope.$broadcast('scroll.refreshComplete');
                  $timeout(function(){
                        $ionicLoading.hide();
-                 },1000);
+                 },500);
              }
           });
       }
-
 
 
      $scope.setActive = function(i){
@@ -100,7 +88,7 @@ angular.module('visuo.controllers', [])
        if($scope.fail == true){
          $scope.doRefresh();
        }
-       $('#slider').fadeOut(50,function(){
+       $('#slider').fadeOut(20,function(){
            $('#slider').fadeIn(200);
        });
 
@@ -111,4 +99,5 @@ angular.module('visuo.controllers', [])
         console.log("SwipeFrom:"+$scope.myActiveSlide);;
       }
 
+     getDeviceInitial();
 });
